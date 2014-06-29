46f7ec2540ce92679b0cc6b61a5bac976e52a86e
Brian <asuuuuskuo@gmail.com>
2014-06-29 22:18:59 +0800

    Final project for CS50

---
+++ includes/constants.php
@@ -0,0 +1,15 @@
+<?php
+
+    // your database's name
+    define("DATABASE", "final");
+
+    // your database's password
+    define("PASSWORD", "KWV%Bf=KxpRxnAw3Th#MZvd");
+
+    // your database's server
+    define("SERVER", "localhost");
+
+    // your database's username
+    define("USERNAME", "brian724080");
+
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>HAI</title>
+	</head>
+	<body>
+		<a href="login.php">Log In</a>
+	</body>
+</html>
